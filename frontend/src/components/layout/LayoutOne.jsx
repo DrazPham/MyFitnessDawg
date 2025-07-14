570dc2c8d7639e189935a61e724e6abc1c42b5dc
DrazPham <drazpham08@gmail.com>
2025-07-14 12:33:47 +0700

    test layoutone

--- frontend/src/components/layout/LayoutOne.jsx
+++ frontend/src/components/layout/LayoutOne.jsx
@@ -1,13 +1,43 @@
+import { useContext,useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
+import { getDoc, doc } from "firebase/firestore";
+import { db } from "src/firebase/index.jsx";
+import { getAuth } from "firebase/auth";
 import Footer from "components/footer/main";
 import Header from "components/header/main";
+import UserInfoContext from "components/functions/UserInfoContext";
+
 function LayoutOne() {
+	const [userInfo, setUserInfo] = useState(null);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+    const userID = localStorage.getItem("userID");
+	if (!userID) {
+		console.warn("⚠️ Không tìm thấy userID trong localStorage");
+    return;
+    }
+	const fetchUserInfo = async () => {
+		try {
+			const userDoc = await getDoc(doc(db, "users", userID));
+			if (userDoc.exists()) {
+			setUserInfo(userDoc.data());
+			}
+		} catch (error) {
+			console.error("Lỗi khi tải user info:", error);
+		} finally {
+			setLoading(false);
+		}
+	};
+
+		fetchUserInfo();
+	}, []);
 	return (
-		<>
+		<UserInfoContext.Provider value={{ userInfo, setUserInfo }}>
 			<Header />
 			<Outlet />
 			<Footer />
-		</>
+		</UserInfoContext.Provider>
 	);
 }
 
